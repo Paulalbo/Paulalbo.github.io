71b92da6d8b27dc498a2af12cb1e86e969c70e74
Paulalbo <65129276+Paulalbo@users.noreply.github.com>
2022-11-27 23:12:29 +0100

    adjust styling/new hero blender animation

--- script/features/vertical-scroll.js
+++ script/features/vertical-scroll.js
@@ -9,7 +9,7 @@ function verticalScroll () {
     let sections = gsap.utils.toArray(".section-about__box, .section-about__headline");
 
     gsap.to(sections, {
-      xPercent: -100 * (sections.length - 1),
+      xPercent: -112 * (sections.length - 1),
       ease: "none",
       scrollTrigger: {
         trigger: ".section-about",
